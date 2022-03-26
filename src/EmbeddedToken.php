80acf13caec12aa938f6c2d3412467923150155a
Victor Aparecido Barbato Rossi <victorap93@gmail.com>
2022-03-26 09:11:55 -0300

    Organização de projeto e para uso da estrutura do composer.

---
+++ src/EmbeddedToken.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace victorap93\PowerBiEmbedded;
+
+class EmbeddedToken
+{
+    public function getEmbeddedToken($workspace_id, $report_id, $ms_token, $params = [])
+    {
+        $client = new \GuzzleHttp\Client();
+        $request = $client->request('POST', "https://api.powerbi.com/v1.0/myorg/groups/$workspace_id/reports/$report_id/GenerateToken", [
+            'headers' => [
+                "Authorization" => "Bearer $ms_token",
+                "Content-Type" => "application/json"
+            ],
+            'json' => $params
+        ]);
+
+        return $request->getStatusCode() === 200 ? json_decode((string) $request->getBody()) : false;
+    }
+}
---
+++ src/MSToken.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace victorap93\PowerBiEmbedded;
+
+class MSToken
+{
+    public function getMSTokenBySecret($tenant_id, $client_id, $client_secret)
+    {
+        $client = new \GuzzleHttp\Client();
+        $request = $client->request('GET', "https://login.microsoftonline.com/$tenant_id/oauth2/token", [
+            'form_params' => [
+                'grant_type' => 'client_credentials',
+                'resource' => 'https://analysis.windows.net/powerbi/api',
+                'client_id' => $client_id,
+                'client_secret' => $client_secret
+            ]
+        ]);
+
+        return $request->getStatusCode() === 200 ? json_decode((string) $request->getBody()) : false;
+    }
+
+    public function getMSTokenByCredentials($tenant_id, $client_id, $username, $password)
+    {
+        $client = new \GuzzleHttp\Client();
+        $request = $client->request('GET', "https://login.microsoftonline.com/$tenant_id/oauth2/token", [
+            'form_params' => [
+                'grant_type' => 'password',
+                'resource' => 'https://analysis.windows.net/powerbi/api',
+                'client_id' => $client_id,
+                'username' => $username,
+                'password' => $password
+            ]
+        ]);
+
+        return $request->getStatusCode() === 200 ? json_decode((string) $request->getBody()) : false;
+    }
+}
